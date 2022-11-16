d944e1554c7b2ca6ed2cb50155e236c203931b90
munifuse24@gmail.com <munifuse24@gmail.com>
2022-11-16 23:14:00 +0600

    3rd

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-const milestoneData = JSON.parse(data);
+const milestoneData = JSON.parse(data).data;
+
+function load() {
+  const milestones = document.querySelector('.milestones');
+  milestones.innerHTML = `${milestoneData.map(function(milestone) {
+    return `<div class="milestones">
+              <div class="milestone border-b">
+                <div class="flex">
+                  <div class="checkbox"><input type="checkbox" /></div>
+                  <div>
+                    <p>
+                      ${milestone.name}
+                      <span><i class="fas fa-chevron-down"></i></span>
+                    </p>
+                  </div>
+                </div>
+                ${milestone.modules.map((module) => {
+                  return `<div class="hidden_panel">
+                                    <div class="module border-b">
+                                      <p>${module.name}</p>
+                                    </div>
+                                  </div>`
+                }).join('')}
+              </div>
+            </div>`
+  }).join('')}`
+}
+
+load()
